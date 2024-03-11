bff4b1dfeac1c975272548b0197b1765fab0aa02
Leandro Montedoro <lmontedoro@itx.com>
2024-03-11 10:12:26 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import aws_cdk as cdk
+from pipeline.pipeline_stack import PipelineStack
+
+
+app = cdk.App()
+PipelineStack(
+    app,
+    "PipelineStack",
+)
+
+app.synth()
